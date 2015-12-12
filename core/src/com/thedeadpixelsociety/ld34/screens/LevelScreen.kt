e1d0c1d02332866625175b6374a2d350503a097c
deadpixelsociety <matt@thedeadpixelsociety.com>
2015-12-11 22:34:09 -0500

    Prototyping

--- core/src/com/thedeadpixelsociety/ld34/screens/LevelScreen.kt
+++ core/src/com/thedeadpixelsociety/ld34/screens/LevelScreen.kt
@@ -4,11 +4,11 @@ import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
-import com.badlogic.gdx.physics.box2d.World
+import com.badlogic.gdx.physics.box2d.*
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.thedeadpixelsociety.ld34.GameServices
 import com.thedeadpixelsociety.ld34.TimeKeeper
+import kotlin.properties.Delegates
 
 class LevelScreen() : GameScreenImpl() {
     val batch by lazy { GameServices[SpriteBatch::class] }
@@ -17,9 +17,21 @@ class LevelScreen() : GameScreenImpl() {
     val camera = OrthographicCamera()
     val viewport = FitViewport(100f, 100f, camera)
     val world = World(Vector2(0f, 9.8f), false)
+    var player by Delegates.notNull<Body>()
 
     override fun show() {
+        val bodyDef = BodyDef()
+        bodyDef.position.set(50f, 50f)
+        bodyDef.type = BodyDef.BodyType.DynamicBody
 
+        player = world.createBody(bodyDef)
+
+        val fixtureDef = FixtureDef()
+        fixtureDef.shape = CircleShape()
+        fixtureDef.shape.radius = 1f
+        fixtureDef.density = 1f
+
+        player.createFixture(fixtureDef)
     }
 
     override fun resize(width: Int, height: Int) {
